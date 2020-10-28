26402849321d7aa49c0376098bb90ec769394c93
albasychris <albasychris@gmail.com>
2020-10-28 20:52:39 +1300

    separate playerShip functions into separate folder and file

--- client/components/Battlefield.jsx
+++ client/components/Battlefield.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './battlefield.css'
-
+import {
+  onKeyDown,
+  onKeyUp
+} from '../helpers/playerShip'
 let keyPressStates = {
   up: false,
   down: false,
@@ -14,85 +17,14 @@ let leftInterval
 let rightInterval
 
 class Battlefield extends Component {
-  onKeyDown = (event) => {
-
-    switch (event.key) {
-      case "ArrowUp":
-        // only toggle if key hasnt been pressed already
-        if (keyPressStates.up === false) {
-          keyPressStates.up = true
-          // console.log("up");
-          upInterval = setInterval(() => {
-            $(".playerShip").animate({ top: "-=10px" }, {duration: 10, queue: false})
-          }, 30)
-        }
-        break;
-
-      case "ArrowDown":
-        // only toggle if key hasnt been pressed already
-        if (keyPressStates.down === false) {
-          keyPressStates.down = true
-          // console.log("down");
-          downInterval = setInterval(() => {
-            $(".playerShip").animate({ top: "+=10px" }, {duration: 10, queue: false})
-          }, 30)
-        }
-        break;
-      case "ArrowLeft":
-        if(keyPressStates.left === false){
-          keyPressStates.left = true
-          // console.log("left")
-          leftInterval = setInterval(() => {
-            $(".playerShip").animate({ left: "-=10px" }, {duration: 10, queue: false})
-          }, 30)
-        }
-        break;
-      case "ArrowRight":
-        if(keyPressStates.right === false){
-          keyPressStates.right = true
-          // console.log("right")
-          rightInterval = setInterval(() => {
-            $(".playerShip").animate({ left: "+=10px" }, {duration: 10, queue: false})
-          }, 30)
-        }
-    
-        break;
-    }
-  }
-  onKeyUp = (event) => {
-    switch (event.key) {
-      case "ArrowUp":
-        // only toggle if key hasnt been pressed already
-
-        keyPressStates.up = false
-        // console.log("up");
-        clearInterval(upInterval)
-        break;
-
-      case "ArrowDown":
-        // console.log("down");
-        keyPressStates.down = false
-        clearInterval(downInterval)
-        break;
-      case "ArrowLeft":
-        // console.log("left");
-        keyPressStates.left = false
-        clearInterval(leftInterval)
-        break;
-      case "ArrowRight":
-        // console.log("right");
-        keyPressStates.right = false
-        clearInterval(rightInterval)
-        break;
-    }
-  }
+ 
   componentDidMount() {
-    document.addEventListener("keydown", this.onKeyDown)
-    document.addEventListener("keyup", this.onKeyUp)
+    document.addEventListener("keydown", onKeyDown)
+    document.addEventListener("keyup", onKeyUp)
   }
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.onKeyDown)
-    document.removeEventListener("keyup", this.onKeyUp)
+    document.removeEventListener("keydown", onKeyDown)
+    document.removeEventListener("keyup", onKeyUp)
   }
 
   render() {
---
+++ client/helpers/playerShip.js
@@ -0,0 +1,84 @@
+let keyPressStates = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+
+let upInterval
+let downInterval
+let leftInterval
+let rightInterval
+
+export const onKeyDown = (event) => {
+
+  switch (event.key) {
+    case "ArrowUp":
+      // only toggle if key hasnt been pressed already
+      if (keyPressStates.up === false) {
+        keyPressStates.up = true
+        // console.log("up");
+        upInterval = setInterval(() => {
+          $(".playerShip").animate({ top: "-=10px" }, {duration: 10, queue: false})
+        }, 30)
+      }
+      break;
+
+    case "ArrowDown":
+      // only toggle if key hasnt been pressed already
+      if (keyPressStates.down === false) {
+        keyPressStates.down = true
+        // console.log("down");
+        downInterval = setInterval(() => {
+          $(".playerShip").animate({ top: "+=10px" }, {duration: 10, queue: false})
+        }, 30)
+      }
+      break;
+    case "ArrowLeft":
+      if(keyPressStates.left === false){
+        keyPressStates.left = true
+        // console.log("left")
+        leftInterval = setInterval(() => {
+          $(".playerShip").animate({ left: "-=10px" }, {duration: 10, queue: false})
+        }, 30)
+      }
+      break;
+    case "ArrowRight":
+      if(keyPressStates.right === false){
+        keyPressStates.right = true
+        // console.log("right")
+        rightInterval = setInterval(() => {
+          $(".playerShip").animate({ left: "+=10px" }, {duration: 10, queue: false})
+        }, 30)
+      }
+  
+      break;
+  }
+}
+export const onKeyUp = (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      // only toggle if key hasnt been pressed already
+
+      keyPressStates.up = false
+      // console.log("up");
+      clearInterval(upInterval)
+      break;
+
+    case "ArrowDown":
+      // console.log("down");
+      keyPressStates.down = false
+      clearInterval(downInterval)
+      break;
+    case "ArrowLeft":
+      // console.log("left");
+      keyPressStates.left = false
+      clearInterval(leftInterval)
+      break;
+    case "ArrowRight":
+      // console.log("right");
+      keyPressStates.right = false
+      clearInterval(rightInterval)
+      break;
+  }
+}
