70dcdbcbdba5d6ab9a79a5c1cc62c08bb52dfde9
albasychris <albasychris@gmail.com>
2020-10-29 21:29:53 +1300

    implement graphQL addScore

--- client/components/Endgame.jsx
+++ client/components/Endgame.jsx
@@ -1,25 +1,54 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { exitGame } from '../store/actions/gamestate'
 import './endgame.css'
 import Button from 'react-bootstrap/Button'
 import { getScore, resetScore } from '../helpers/playerShip'
-class Endgame extends Component {
-  componentWillUnmount () {
-    resetScore()
-  }
-  render() {
-    return (
-      <div className="scorescreen" >
-        <div>
-          <p>The game has ended. Here is your score: { getScore() }</p>
-          <input type="text" placeholder="Enter your name" />
-          <Button onClick={() => this.props.exitGame()} variant="dark">Save Score</Button>
-        </div>
+import { useMutation, gql } from '@apollo/client';
 
-      </div>
-    )
+
+const Endgame = (props) => {
+  const SCORE = gql`
+  mutation AddScore($name: String!, $score: Float!){
+  addScore(name:$name,score:$score) {
+    id
+    name
+    score
   }
+}
+`;
+  const [addScore, { data }] = useMutation(SCORE)
+  const score = getScore()
+  const [name, setName] = useState('name')
+
+  const handleChange = (evt) => {
+    setName(evt.target.value)
+  }
+  const handleClick = () => {
+    if(name === 'name'){
+      return 
+    }
+    addScore({
+      variables: { name: name, score: score}
+    })
+    props.exitGame()
+  }
+  useEffect(() => {
+    return () => {
+      resetScore()
+    }
+  }, [])
+  return (
+    <div className="scorescreen" >
+      <div>
+        <p>The game has ended. Here is your score: {score}</p>
+        <input onChange={handleChange} type="text" placeholder="Enter your name" />
+        <Button onClick={handleClick} variant="dark">Save Score</Button>
+      </div>
+
+    </div>
+  )
+
 }
 
 const mapDispatchToProps = {
