0d145742173a82e2fa95fe3b3f4ac287f6a77e0c
albasychris <albasychris@gmail.com>
2020-10-29 17:31:27 +1300

    implement graphql getScores query

--- server/db/fn/scores.js
+++ server/db/fn/scores.js
@@ -1,12 +1,16 @@
 const connection = require('../connection')
 const snakecaseKeys = require('snakecase-keys')
 
-function getScores (db = connection) {
-  return db('scores').select()
+function getScores(db = connection) {
+  return db('scores').select().catch(err => {
+    console.error(err)
+  })
 }
 
-function addScore (scoreData, db = connection) {
-  return db('scores').insert(snakecaseKeys(scoreData))
+function addScore(scoreData, db = connection) {
+  return db('scores').insert(snakecaseKeys(scoreData)).catch(err => {
+    console.error(err)
+  })
 }
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -7,3 +7,55 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
 module.exports = server
+const {
+  getScores,
+  addScore
+} = require("./db/fn/scores")
+
+var { graphqlHTTP } = require('express-graphql');
+var {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLNonNull
+} = require('graphql');
+
+const ScoresType = new GraphQLObjectType({
+  name: 'score',
+  description: 'This represents a score of a user',
+  fields: () => ({
+    id: {type: GraphQLNonNull(GraphQLInt)},
+    name: {type: GraphQLNonNull(GraphQLString)},
+    score: {type: GraphQLNonNull(GraphQLFloat)}
+  })
+})
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    scores: {
+      type: GraphQLList(ScoresType),
+      description: 'A list of scores',
+      resolve: () => { 
+        let scores = getScores()
+        return scores
+       }
+    }
+  })
+})
+
+// Construct a schema, using GraphQL schema language
+const schema = new GraphQLSchema({
+  query: RootQueryType
+})
+
+
+
+server.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
+}));
