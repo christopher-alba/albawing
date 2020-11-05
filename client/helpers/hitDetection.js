b4be065d372737881039ff7980b6fcd88db2bdcf
albasychris <albasychris@gmail.com>
2020-11-05 20:31:08 +1300

    add explosions for bullet hits

--- client/helpers/hitDetection.js
+++ client/helpers/hitDetection.js
@@ -22,6 +22,7 @@ export const checkEnemyHit = () => {
             // clear the bulletq
             try {
               arena.removeChild(bullet)
+              bulletExplode(bulletBox)
             } catch (err) {
             }
 
@@ -36,6 +37,7 @@ export const checkEnemyHit = () => {
         let enemyBossBox = enemyBoss.getBoundingClientRect()
         if(bulletBox.top < enemyBossBox.top + enemyBossBox.height && bulletBox.left > enemyBossBox.left && bulletBox.right < enemyBossBox.right){
           bullet.remove()
+          bulletExplode(bulletBox)
           damageEnemyBoss(1)
           console.log(getBossHealth());
         }
@@ -62,8 +64,23 @@ export const checkPlayerHit = () => {
       let playerBox = document.getElementsByClassName("playerShip")[0].getBoundingClientRect()
       if((bulletBox.top + bulletBox.height > playerBox.top && bulletBox.top < playerBox.top + playerBox.height && bulletBox.left > playerBox.left + playerBox.width / 3 && bulletBox.right < playerBox.left + playerBox.width - playerBox.width / 3)){
         arena.removeChild(bullet)
+        bulletExplode(bulletBox)
         reducePlayerHealth(10)
       }
     }
   }
+}
+
+// call this function whenever a bullet has hit its target
+const bulletExplode = (bulletBox) => {
+  // create explosion
+  let explosion = document.createElement("div")
+  explosion.style.top = bulletBox.top + "px"
+  explosion.style.left = bulletBox.left + "px"
+  explosion.classList.add("bulletExplode")
+  
+  let arena = document.getElementsByClassName("mainContainer")[0]
+  arena.appendChild(explosion)
+  explosion.animate({opacity: 0}, {duration: 500, queue: false})
+  setTimeout(() => explosion.remove(), 500)
 }
